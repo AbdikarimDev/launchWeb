887c7a28f8563b126e1c3803227a28d224d11035
AbdikarimDev <abdikarim.myworks@gmail.com>
2026-08-12 11:49:00 +0300

    first update

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- all components -->
+   <div>
+    <!-- TOPBAR -->
+  <div>
+    <Topbar/>
+  </div>
+   </div>
+</template>
+
+<script>
+import Topbar from './components/Topbar.vue';
+export default {
+    components:{Topbar}
+    
+  }
+</script>
+ 
---
+++ src/components/Topbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex justify-around items-center text-white my-4">
+    <div class="flex gap-10 justify-between items-center">
+      <!-- logo -->
+      <div class="flex  gap-2">
+        <img src="../../public/favicon.svg" alt="" />
+        <h1 class="font-bold text-[18px]">Launch UI</h1>
+      </div>
+      <!-- navigations -->
+      <div class="flex gap-10">
+        <a href="home" class="font-[400] text-[14px]">Getting started</a>
+        <a href="components" class="font-[400] text-[14px]">Components</a>
+        <a href="documentation" class="font-[400] text-[14px]">Documentation</a>
+        <a href="QA" class="font-[400] text-[14px]">QA</a>
+      </div>
+    </div>
+    <!-- Buttons -->
+     <div>
+        <button class="font-[500] text-[14px] bg-black px-4 py-3 ">Sign in</button>
+        <button class="font-[500] text-[14px] text-black bg-white px-4 py-3 rounded-[8px]">Get started</button>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
